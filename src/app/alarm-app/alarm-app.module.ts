2d6f00a65bbf24f64d939b786964d3528e79093a
CamiloCastro-MISO <bc.castro@uniandes.edu.co>
2024-09-24 21:17:36 -0500

    register page finished

--- src/app/alarm-app/alarm-app.module.ts
+++ src/app/alarm-app/alarm-app.module.ts
@@ -5,11 +5,15 @@ import { AlarmAppRoutingModule } from './alarm-app-routing.module';
 import { MaterialModule } from '../material.module';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RegisteredComponent } from './pages/registered/registered.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    RegisteredComponent
   ],
   imports: [
     CommonModule,
